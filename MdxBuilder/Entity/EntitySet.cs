4f305be53b7ea0f4d50d4ac8d1350dd80c2695a3
Ortholite <ortholite-k@yandex.ru>
2021-05-04 13:35:15 +0300

    ~ Additional contructor for EntitySet.

--- MdxBuilder/Entity/EntitySet.cs
+++ MdxBuilder/Entity/EntitySet.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using MdxBuilder.Entity.Abstract;
 
@@ -12,6 +13,14 @@ namespace MdxBuilder.Entity
 
         public EntitySet()
             => items = new List<TEntity>();
+
+        [SuppressMessage(
+            "Usage",
+            "CC0057:Unused parameters",
+            Justification = "The UnusedParametersAnalyzer bug."
+        )]
+        public EntitySet(IEnumerable<TEntity> entities)
+            => items = entities.ToList();
 
         public override string ToString()
             => string.Join(", ", items.Select(i => i.ToString()));
