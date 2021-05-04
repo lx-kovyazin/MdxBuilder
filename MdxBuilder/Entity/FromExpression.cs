19d2ba36f079f14f074d6ef2430ca58ea9d6d8c2
Ortholite <ortholite-k@yandex.ru>
2021-05-04 06:33:40 +0300

    + Moving classes to new files. ~ Made some classes sealed.

--- MdxBuilder/Entity/FromExpression.cs
+++ MdxBuilder/Entity/FromExpression.cs
@@ -1,41 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MdxBuilder.Builder;
+﻿using MdxBuilder.Builder;
 using MdxBuilder.Entity.Abstract;
 
 namespace MdxBuilder.Entity
 {
-    public abstract class VariableEntity<T>
-        : AbstractEntity
-    {
-    }
-
-    public class Cube
-        : UniqueEntity
-    {
-        public override string ToString()
-            => UniqueName;
-    }
-
-    public class SubCube
-        : BuildableEntityOfBuildableEntity<
-            SubCube,
-            SubCubeBuilder,
-            Mdx.Query,
-            Mdx.QueryBuilder
-            >
-    {
-        public Cube Cube;
-    }
-
-    public class FromExpression
+    public sealed class FromExpression
         : BuildableEntityOfBuildableEntity<
             FromExpression,
             FromExpressionBuilder,
             SubCube,
             SubCubeBuilder
         >
-    {
-    }
+    { }
 }
---
+++ MdxBuilder/Entity/SubCube.cs
@@ -0,0 +1,16 @@
+﻿using MdxBuilder.Builder;
+using MdxBuilder.Entity.Abstract;
+
+namespace MdxBuilder.Entity
+{
+    public sealed class SubCube
+        : BuildableEntityOfBuildableEntity<
+            SubCube,
+            SubCubeBuilder,
+            Mdx.Query,
+            Mdx.QueryBuilder
+            >
+    {
+        public Cube Cube;
+    }
+}
